6765b0ca04b03c0dfba8f347f85e54a3608acb11
Richard <respino.sp@mysummitps.org>
2016-12-16 17:33:12 +0000

    update to how to page and two new pages

---
+++ calc.js
@@ -0,0 +1,25 @@
+// JavaScript File
+var button = document.getElementById("submitButton");
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
+var display = document.getElementById("display");
+var dropdown = document.getElementById("dropdown");
+var answer;
+
+button.addEventListener("click", doMath);
+
+function doMath(){
+    if (dropdown.value == "+"){
+        answer = parseInt(input1.value) + parseInt(input2.value);
+        display.innerHTML = answer;
+    }else if(dropdown.value == "-"){
+        answer = parseInt(input1.value) - parseInt(input2.value);
+        display.innerHTML = answer;
+    }else if(dropdown.value == "*"){
+        answer = parseInt(input1.value) * parseInt(input2.value);
+        display.innerHTML = answer;
+    }else if(dropdown.value == "/"){
+        answer = parseInt(input1.value) / parseInt(input2.value);
+        display.innerHTML = answer; 
+    }
+}
